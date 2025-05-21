849034a76a25adaa1da26b5fd195644f0db92b81
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2025-05-21 23:16:24 +0530

    refactor: adjust spacing and font sizes in Profile component for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ function App() {
 
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col h-full w-full gap-10 md:max-w-4/7 lg:max-w-4/7 px-8 py-20">
+      <div className="flex flex-col h-full w-full gap-8 md:max-w-4/7 lg:max-w-4/7 px-8 py-20">
         <Profile/>
-        
       </div>
     </div>
   )
--- src/components/page-section/profile.tsx
+++ src/components/page-section/profile.tsx
@@ -3,11 +3,11 @@ function Profile() {
     <div className='h-full w-full flex justify-center'>
       <div className='h-full w-full flex flex-col md:flex-row items-center justify-center gap-5 md:gap-0.5'>
         <div className='flex items-center justify-center'>
-          <div className='bg-gray-600 rounded-full h-52 w-52 flex justify-center items-center'>image</div>
+          <div className='bg-gray-600 rounded-full h-36 w-36 flex justify-center items-center'>image</div>
         </div>
-        <div className='w-full h-full text-center md:text-left md:pl-10 space-y-5'>
-          <h1 className='text-4xl md:text-6xl font-bold'>Hii!!, I'm Bhavesh</h1>
-          <h3 className='text-2xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, molestias.</h3>
+        <div className='w-full h-full text-center md:text-left md:pl-10 space-y-3'>
+          <h1 className='text-3xl md:text-5xl font-bold'>Hii!!, I'm Bhavesh</h1>
+          <h3 className='text-xl font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, molestias.</h3>
         </div>
       </div>
     </div>
