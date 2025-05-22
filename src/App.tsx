11af8aa223bdc7e6742848b19cff4a9d404dbb23
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2025-05-22 21:20:51 +0530

    feat: add Skill and Project components, implement Badge UI for skills display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Profile from "./components/page-section/profile";
 import About from "./components/page-section/about";
+import Skill from "./components/page-section/skill";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div className="flex flex-col h-full w-full gap-8 md:max-w-4/7 lg:max-w-4/7 px-8 py-20">
         <Profile/>
         <About/>
+        <Skill/>
       </div>
     </div>
   )
