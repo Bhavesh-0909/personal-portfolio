716286c6eb37e21f2db3ce2393513732eb5d5138
Bhavesh Choudhary <133791708+Bhavesh-0909@users.noreply.github.com>
2025-07-15 06:04:04 +0000

    feat: emailjs added to contact form

--- src/components/page-section/contact.tsx
+++ src/components/page-section/contact.tsx
@@ -1,21 +1,42 @@
-import { Badge } from '@/components/ui/badge'
+import { Badge } from '@/components/ui/badge';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function ContactPage() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(process.env.EMAILJS_SERVICE_ID as string, process.env.EMAILJS_TEMPLATE_ID as string, form.current as HTMLFormElement, {
+        publicKey: process.env.EMAILJS_PUBLIC_KEY as string,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div>
       <div className="h-full w-full flex flex-col justify-center items-center gap-3 mt-4 md:mt-8">
         <h3><Badge variant="default" className='text-sm'>Contact</Badge></h3>
         <p className='sm:text-5xl text-3xl tracking-tighter font-bold'>Get in touch</p>
         <div className='w-full max-w-md p-4 bg-slate-900 rounded-2xl shadow-md'>
-          <form className='flex flex-col gap-2' name='form' action="" method="POST">
+          <form className='flex flex-col gap-2' name='form' ref={form} onSubmit={sendEmail}>
             <div>
-              <input type="text" id="name" name="SingleLine" placeholder='Name' className='bg-slate-950 w-full py-2 px-2 rounded-[12px]' required />
+              <input type="text" id="name" name="name" placeholder='Name' className='bg-slate-950 w-full py-2 px-2 rounded-[12px]' required />
             </div>
             <div>
-              <input type="email" id="email" name="Email" placeholder='Email' className='bg-slate-950 w-full py-2 px-2 rounded-[12px]' required />
+              <input type="email" id="email" name="email" placeholder='Email' className='bg-slate-950 w-full py-2 px-2 rounded-[12px]' required />
             </div>
             <div>
-              <textarea id="message" name="MultiLine" placeholder='Message' className='bg-slate-950 w-full py-2 px-2 h-24 rounded-[12px]' required></textarea>
+              <textarea id="message" name="message" placeholder='Message' className='bg-slate-950 w-full py-2 px-2 h-24 rounded-[12px]' required></textarea>
             </div>
             <button type="submit" className='p-2 bg-indigo-500 rounded-[12px] cursor-pointer'>Send</button>
           </form>
